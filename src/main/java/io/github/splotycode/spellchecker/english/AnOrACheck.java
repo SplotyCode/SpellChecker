b8cb6f3170eae3afd194cb2fb2eb79ac0937e2ec
david <davidscandurra@gmail.com>
2019-04-17 21:20:04 +0200

    AnOrACheck & EnglishLanguage

---
+++ src/main/java/io/github/splotycode/spellchecker/english/AnOrACheck.java
@@ -0,0 +1,48 @@
+package io.github.splotycode.spellchecker.english;
+
+import io.github.splotycode.mosaik.util.AlmostBoolean;
+import io.github.splotycode.spellchecker.check.Check;
+import io.github.splotycode.spellchecker.check.ProblemCollector;
+import io.github.splotycode.spellchecker.check.visitor.ElementVisitor;
+import io.github.splotycode.spellchecker.check.visitor.Visitor;
+import io.github.splotycode.spellchecker.dictfile.CaseDictFile;
+import io.github.splotycode.spellchecker.element.Word;
+
+import static io.github.splotycode.mosaik.util.AlmostBoolean.*;
+
+public class AnOrACheck extends Check {
+
+    private CaseDictFile aDict = new CaseDictFile("en/a.txt");
+    private CaseDictFile anDict = new CaseDictFile("en/an.txt");
+
+    @Override
+    public Visitor visitor() {
+        return new ElementVisitor() {
+            @Override
+            public void visitWord(ProblemCollector collector, Word word) {
+                if (word.match(NO, "an", "a")) {
+                    Word next = word.nextWord();
+                    if (next == null) {
+                        collector.addProblem(word, "Unusually end");
+                    } else {
+                        AlmostBoolean shouldBeA = aDict.exists(next) ? YES : MAYBE;
+                        if (anDict.exists(next)) {
+                            if (shouldBeA == YES) {
+                                /* This word allows both determiner */
+                                return;
+                            }
+                            shouldBeA =  NO;
+                        }
+                        if (shouldBeA == MAYBE) {
+                            shouldBeA = fromBoolean(!next.firstLetter().isVocal());
+                        }
+                        String correct = shouldBeA.decide(true) ? "a" : "an";
+                        if (!word.asString().equalsIgnoreCase(correct)) {
+                            collector.addProblem(word, "Should be " + correct);
+                        }
+                    }
+                }
+            }
+        };
+    }
+}
---
+++ src/main/java/io/github/splotycode/spellchecker/english/EnglishLanguage.java
@@ -0,0 +1,29 @@
+package io.github.splotycode.spellchecker.english;
+
+import io.github.splotycode.spellchecker.Language;
+import io.github.splotycode.spellchecker.check.defaults.*;
+
+public class EnglishLanguage extends Language {
+
+    public EnglishLanguage() {
+        checks.add(new DoubleWhitespaceCheck());
+        checks.add(new DoubleWordCheck());
+        checks.add(new SentenceCapitalisationCheck());
+        checks.add(new WhitespaceAfterNewLineCheck());
+        checks.add(new WhitespacingCheck());
+
+        checks.add(new AnOrACheck());
+        checks.add(new ContractionsCheck());
+    }
+
+    @Override
+    public boolean isVocal(char character) {
+        char lc = Character.toLowerCase(character);
+        return lc == 'a' || lc == 'e' || lc == 'i' || lc == 'o' || lc == 'u';
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "English";
+    }
+}
